0e750c88c1f1e14f5ac45e076727dd421893558c
Gonzalo Migliardi <gonzalomigliardi9@gmail.com>
2022-10-11 18:15:56 -0300

    rentrega

--- pre-entrega/script.js
+++ pre-entrega/script.js
@@ -1,26 +1,67 @@
+let cantidad_de_alumnos = parseInt(prompt("Cantidad de alumnos a promediar"));
 let alumno;
 let nota1;
 let nota2;
-let suma;
 let notas;
 
-for (let i = 0 ; i < 1 ; i++){
-    alumno = prompt("Nombre del alumno");
-    nota1 = parseInt(prompt("Ingrese nota 1"));
-    nota2 = parseInt(prompt("Ingrese nota 2"));
-
-    suma = nota1 + nota2
-
-    notas = suma / 2
-
-    if (notas >= 7){
-        console.log("La nota de", alumno , "es", notas);
-        console.log("Usted esta aprobado");
+if(typeof (cantidad_de_alumnos) !== "number"){
+    console.log("Pone un numero");
+}
+else {
+    for (let i = 0 ; i < cantidad_de_alumnos; i++){
+        alumno = prompt("Nombre del alumno");
+        nota1 = obtenerNota(1);
+       
+        nota2 = obtenerNota(2);
+       
+        notas = promedio(nota1, nota2);
+        
+        if (aprobado(notas)){
+            console.log("La nota de", alumno , "es", notas);
+            console.log("Usted esta aprobado");
+        }
+    
+        else{
+            console.log("La nota de ", alumno , "es", notas);
+            console.log("Usted esta desaprobado");
+        }
+    
     }
+}
 
-    else{
-        console.log("La nota de ", alumno , "es", notas);
-        console.log("Usted esta desaprobado");
+
+function obtenerNota(examenNum){
+    let nota;
+    nota = prompt("Ingrese nota " + examenNum);
+        if(isNaN(nota)){
+            while(isNaN(nota)){
+                nota = prompt("Ingrese nota " + examenNum + " (Ingrese un numero)");
+                if (isNaN(nota)){
+                    nota = parseInt(nota);
+                }
+            }
+        }
+        else {
+            nota = parseInt(nota);
+        }
+        
+        return nota;
+}
+
+function promedio(nota1, nota2){
+    let suma = 0;
+    suma = parseInt(nota1) + parseInt(nota2);
+    
+        suma = suma / 2;
+        
+        return suma;
+}
+
+function aprobado(nota){
+    if (nota >= 7){
+        return true;
     }
-
+    else {
+        return false;
+    }
 }
